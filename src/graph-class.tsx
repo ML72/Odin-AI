c18ace796dd4576fef9400a4e66c944b7e813fd3
malti <malti@stanford.edu>
2025-02-15 10:26:56 -0800

    Added the knowledge graph and graph class code

---
+++ src/graph-class.tsx
@@ -0,0 +1,20 @@
+class N {
+    title: string;
+    importance: number;
+
+    constructor(title: string, importance: number) {
+        this.title = title;
+        this.importance = importance;
+    }
+}
+
+class Edge {
+    title: string;
+    weight: number;
+
+    constructor(title: string, weight: number) {
+        this.title = title;
+        this.weight = weight;
+    }
+
+}
