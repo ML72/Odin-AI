7a578d9a2b04232739066962a795379ff47bb17f
malti <malti@stanford.edu>
2025-02-15 23:42:12 -0800

    cleans up edges and weights

--- src/service/graphs/combine_graphs.ts
+++ src/service/graphs/combine_graphs.ts
@@ -44,7 +44,7 @@ async function combineKeywords(k1: string, k2: string) {
 // Taking in two of your own lecture notes and overlaying them 
 export async function constructSharedGraph(g1: Graph, g2: Graph) {
     const nodeMap = new Map();
-    const mergedEdges = new Set(g1.edges); 
+    const mergedEdges = new Set(); 
 
     let i = 0;
 
@@ -76,6 +76,23 @@ export async function constructSharedGraph(g1: Graph, g2: Graph) {
         mergedEdges.add(new Edge(edge.connection, edge.weight, newSource, newTarget, g2.id, true, 1));
     }
 
+    for (const edge of g1.edges) {
+        const newSource = nodeMap.get(edge.from.title);
+        const newTarget = nodeMap.get(edge.to.title);
+        let added = false;
+        for (const prev_edge of g2.edges) {
+            if ((newSource == nodeMap.get(prev_edge.from.title) && newTarget == nodeMap.get(prev_edge.to.title))||
+            (newSource == nodeMap.get(prev_edge.to.title) && newTarget == nodeMap.get(prev_edge.from.title))) {
+                added = true;
+                let weight = (edge.from.weight*edge.to.weight) / (prev_edge.from.weight*prev_edge.to.weight) * edge.weight;
+                mergedEdges.add(new Edge(prev_edge.connection, weight, newSource, newTarget, g1.id, false, 1));
+            }
+        } 
+        if (!added) {
+            mergedEdges.add(new Edge(edge.connection, edge.weight, newSource, newTarget, g1.id, false, 1));
+        }
+    }
+
     for (let i = 0; i < g1.nodes.length; i++) {
         g1.nodes[i].id = i;
     }
@@ -97,7 +114,41 @@ export async function constructSharedGraph(g1: Graph, g2: Graph) {
     }
 
     let g = new Graph(g1.nodes, edgeList);
-    g.calculateNodeCentrality();
+
 
     return g;
 }
+/*
+const g1_nodes  = [
+    new N("Taxation without representation", "", 1),
+    new N("Sugar Act", "", 2),
+    new N("Stamp Act", "", 3),
+    new N("American Revolution", "", 4),
+]
+
+const g2_nodes = [
+    new N("Great Britain", "", 1),
+    new N("American Revolution", "", 3),
+    new N("Taxation with no representation", "", 0.5),
+    new N("Declaration of Independence", "", 2),
+    new N("Sugar Act", "", 1),
+]
+
+const g1_edges = [
+    new Edge("Led to grievances from colonists", 1, g1_nodes[0], g1_nodes[1]),
+    new Edge("Led to grievances from colonists", 2, g1_nodes[0], g1_nodes[2]),
+]
+
+const g2_edges = [
+    new Edge("They lost in this battle", 3, g2_nodes[0], g2_nodes[1]),
+    new Edge("Root cause of conflict", 0.5, g2_nodes[2], g2_nodes[1]),
+    new Edge("Explicitly addressed in document", 3, g2_nodes[1], g2_nodes[3]),
+    new Edge("This led to grievances from colonists", 1.5, g2_nodes[2], g2_nodes[4])
+]
+
+const g1 = new Graph(g1_nodes, g1_edges);
+const g2 = new Graph(g2_nodes, g2_edges);
+
+const g3 = await constructSharedGraph(g1, g2);
+g3.print();
+*/
