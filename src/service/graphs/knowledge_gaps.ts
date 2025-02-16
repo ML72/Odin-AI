370d25215ec194c5254b60721bffd19aaaf030e5
malti <malti@stanford.edu>
2025-02-15 21:35:27 -0800

    added extra node functionality

--- src/service/graphs/knowledge_gaps.ts
+++ src/service/graphs/knowledge_gaps.ts
@@ -32,25 +32,42 @@ export const knowledgeGaps = async (graph: Graph) => {
     }
 
     for (let i = 0; i < node_map.length; i++) {
-        if (!ground_truth_list[i]) continue;
-        for (const edge of ground_truth_list[i]) {
-            let diff = edge.weight;
-            if (your_understanding_list[i]){
-                for (const my_und of your_understanding_list[i]) {
-                    if (((my_und.to == edge.to && my_und.from == edge.from)
-                    || (my_und.from == edge.to && my_und.to == edge.from))) {
-                        diff = Math.max(0, edge.weight - my_und.weight);
+        if (ground_truth_list[i]) {
+            for (const edge of ground_truth_list[i]) {
+                let diff = edge.weight;
+                if (your_understanding_list[i]){
+                    for (const my_und of your_understanding_list[i]) {
+                        if (((my_und.to == edge.to && my_und.from == edge.from)
+                        || (my_und.from == edge.to && my_und.to == edge.from))) {
+                            diff = Math.max(0, edge.weight - my_und.weight);
+                        }
+                    }
+                }
+                edge.weight += diff*1.5;
+                personal_edges.push(edge);
+            }
+        }
+        if (!your_understanding_list[i]) continue;
+        for (const edge of your_understanding_list[i]) {
+            let exists = false;
+            if (ground_truth_list[i]){
+                for (const g_t of ground_truth_list[i]) {
+                    if ((g_t.to == edge.to && g_t.from == edge.from) ||
+                        (g_t.to == edge.from && g_t.from == edge.to)) {
+                            exists = true;
                     }
                 }
             }
-            edge.weight += diff*1.5;
-            personal_edges.push(edge);
+            if (!exists) {
+                edge.weight *= .1;
+                personal_edges.push(edge);
+            }
         }
     }
     
     return new Graph(node_map, personal_edges);
 }
-/*
+
 const g1_nodes  = [
     new N("Taxation without representation", "", 1, 0),
     new N("Sugar Act", "", 1, 1),
@@ -60,12 +77,12 @@ const g1_nodes  = [
 const g1_edges = [
     new Edge("Led to grievances from colonists1", 2, g1_nodes[0], g1_nodes[1], 1, true, 1),
     new Edge("Led to grievances from colonists", 3, g1_nodes[0], g1_nodes[2], 1, true, 1),
-    new Edge("Led to grievances from colonists", 3, g1_nodes[0], g1_nodes[2], 1, false, 1)
+    new Edge("Led to grievances from colonists dne", 3, g1_nodes[1], g1_nodes[2], 1, false, 1)
 ]
 
 const graph = new Graph(
     g1_nodes, g1_edges
 );
 
-const g3 = await knowledge_gaps(graph);
-g3.print();*/
+const g3 = await knowledgeGaps(graph);
+g3.print();
