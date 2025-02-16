932d663098fd40d5688c3a90c876bb3bdb04eacd
Michael <michael.mingdao.li@gmail.com>
2025-02-15 19:16:01 -0800

    Markdown transform connected

--- src/service/upload.ts
+++ src/service/upload.ts
@@ -1,5 +1,6 @@
 import { transcribeAudio } from "./external/transcribeAudio";
 import { handwrittenToText } from "./external/transcribeWritten";
+import { transformToMarkdown } from "./external/markdownTransform";
 
 
 // The god function which does the WHOLE upload pipeline :)
@@ -11,12 +12,34 @@ export const handleUpload = async (selectedUserPngFile: any, selectedMp3File: an
     if (selectedUserPngFile) {
         userText = await handwrittenToText([selectedUserPngFile]); // Array of single element for now
     }
+    updateProgress(15);
 
     if (selectedMp3File) {
         audioText = await transcribeAudio(selectedMp3File);
     }
+    updateProgress(30);
 
-    // TODO chain more APIs
+    // Step 2: Transform to markdown
+    let userMarkdown: any = null;
+    let audioMarkdown: any = null;
+
+    if (userText) {
+        userMarkdown = await transformToMarkdown(userText);
+    }
+    updateProgress(45);
+
+    if (audioText) {
+        audioMarkdown = await transformToMarkdown(audioText);
+    }
+    updateProgress(60);
+
+    // Step 3: Generate separate graphs
+
+    
+    // Step 4: Combine graphs, if applicable
+
+    
+    // Step 5: Retrieve stats
     
 
 
