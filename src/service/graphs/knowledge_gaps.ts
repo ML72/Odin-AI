c23501c59ad3b331e64a1fbbf7b4fdd7a046f820
malti <malti@stanford.edu>
2025-02-15 18:45:45 -0800

    debugging!

--- src/service/graphs/knowledge_gaps.ts
+++ src/service/graphs/knowledge_gaps.ts
@@ -18,16 +18,30 @@ export const knowledge_gaps = async (graph: Graph) => {
 
     for (const edge of graph.edges) {
         if (edge.ground_truth) {
+            if (!ground_truth_list[edge.from.id]) {
+                ground_truth_list[edge.from.id] = [];
+            }
+            if (!ground_truth_list[edge.to.id]) {
+                ground_truth_list[edge.to.id] = [];
+            }
             ground_truth_list[edge.from.id].push(edge);
             ground_truth_list[edge.to.id].push(edge);
         }
         else {
+            if (!your_understanding_list[edge.from.id]) {
+                your_understanding_list[edge.from.id] = [];
+            }
+            if (!your_understanding_list[edge.to.id]) {
+                your_understanding_list[edge.to.id] = [];
+            }
             your_understanding_list[edge.from.id].push(edge);
             your_understanding_list[edge.to.id].push(edge);
         }
     }
-
+    console.log(ground_truth_list);
+    console.log(your_understanding_list);
     for (let i = 0; i < node_map.length; i++) {
+        if (!ground_truth_list[i]) continue;
         for (const edge of ground_truth_list[i]) {
             let diff = edge.weight;
             for (const my_und of your_understanding_list[i]) {
@@ -43,3 +57,21 @@ export const knowledge_gaps = async (graph: Graph) => {
     
     return new Graph(node_map, personal_edges);
 }
+
+const g1_nodes  = [
+    new N("Taxation without representation", "", 0),
+    new N("Sugar Act", "", 1),
+    new N("Stamp Act", "", 2),
+]
+
+const g1_edges = [
+    new Edge("Led to grievances from colonists1", 0, g1_nodes[0], g1_nodes[1], 1, true),
+    new Edge("Led to grievances from colonists", 0, g1_nodes[0], g1_nodes[2], 1, false),
+]
+
+const graph = new Graph(
+    g1_nodes, g1_edges
+);
+
+const g3 = await knowledge_gaps(graph);
+g3.print();
